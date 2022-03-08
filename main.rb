c32d1d8113370b4fc24791d2b38e57709f4c260f
John Chia <john.chia0106@gmail.com>
2022-03-07 17:46:58 -0800

    init commit

---
+++ main.rb
@@ -0,0 +1,9 @@
+
+   
+require './game.rb'
+require './question.rb'
+require './player.rb'
+
+game = Game.new()
+
+game.start()
---
+++ player.rb
@@ -0,0 +1,8 @@
+class Player
+  attr_accessor :id, :lives
+
+  def initialize(id, lives)
+      @id = id
+      @lives = lives
+  end
+end
